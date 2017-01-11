5c8afff76be33bf4834c1c59ccb7540b73f3ccbb
Christophe Robin <crobin@nekoo.com>
2017-01-11 17:37:56 +0900

    Fix README, add availability management

--- README.md
+++ README.md
@@ -1,38 +1,65 @@
-Role Name
-=========
+Swarm
+=====
 
-A brief description of the role goes here.
+Library and tasks for creating a swarm docker 1.12+
+
+When using the tasks you should make sure that either your managers are already setup or that the leader nodes are
+processed before/at the same time as the managers otherwise the manager join will fail due to the cluster not existing.
 
 Requirements
 ------------
 
-Any pre-requisites that may not be covered by Ansible itself or the role should be mentioned here. For instance, if the role uses the EC2 module, it may be a good idea to mention in this section that the boto package is required.
+Docker must be available on the node itself.
 
 Role Variables
 --------------
 
-A description of the settable variables for this role should go here, including any variables that are in defaults/main.yml, vars/main.yml, and any variables that can/should be set via parameters to the role. Any variables that are read from other roles and/or the global scope (ie. hostvars, group vars, etc.) should be mentioned here as well.
+```yaml
+# this is the url to the docker instance we want to manipulate, default to the unix socket
+swarm_docker_url: ""
+
+# whether we should create a cluster on this node?
+swarm_leader: false
+
+# use to join a cluster as a manager
+swarm_manager: false
+
+# use to join a cluster as a worker
+swarm_worker: false
+
+# when joining a cluster, this should contain a list of nodes with docker available through HTTP (port defaults to 2376
+# if not specified)
+swarm_remote_addrs: []
+
+# on which (address|interface)[:port] the swarm should be listening
+swarm_listen_addr: 0.0.0.0
+
+# advertise address, docker will try to find a decent default
+swarm_advertise_addr: ""
+
+# when set, updates the node availability to match this value, can be either "active", "pause", "drain"
+swarm_availability: "drain"
+```
 
 Dependencies
 ------------
 
-A list of other roles hosted on Galaxy should go here, plus any details in regards to parameters that may need to be set for other roles, or variables that are used from other roles.
+None
 
 Example Playbook
 ----------------
 
 Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
 
-    - hosts: servers
-      roles:
-         - { role: username.rolename, x: 42 }
+```yaml
+- hosts: swarm
+  gather_facts: true
+  become: true
+  roles:
+  - AerisCloud.swarm
+```
 
 License
 -------
 
-BSD
-
-Author Information
-------------------
-
-An optional section for the role authors to include contact information, or a website (HTML is not allowed).
+MIT
--- library/swarm.py
+++ library/swarm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 '''
-Hello?
+Allows one to manipulate a Swarm cluster through ansible directives
 '''
 
 # The MIT License (MIT)
@@ -57,6 +57,7 @@ from httplib import HTTPConnection
 
 import json
 import socket
+import urlparse
 
 # HELPERS
 
@@ -123,30 +124,66 @@ def build_url(args, path):
     if '://' not in url:
         url = 'http://' + url
 
-    return url + path
+    parsed_url = urlparse.urlparse(url)
+    if parsed_url.scheme[0:4] == 'http' and parsed_url.port == None:
+        parsed_url = parsed_url._replace(netloc=parsed_url.netloc + ':2376')
+
+    parsed_url = parsed_url._replace(path=path)
+
+    return parsed_url.geturl()
 
 def fetch_url(url, data=None):
-    """Try to fetch the given URL, return an empty dict on failure"""
+    """Try to fetch the given URL, if data is provided it is json encoded and POSTed"""
+    req = None
+    if data != None:
+        req = Request(url, json.dumps(data), {'Content-Type': 'application/json'})
+    else:
+        req = Request(url)
 
+    f = urlopen(req)
+    res = f.read()
+    code = f.getcode()
+    f.close()
 
-    try:
-        req = None
-        if data != None:
-            req = Request(url, json.dumps(data), {'Content-Type': 'application/json'})
-        else:
-            req = Request(url)
+    if len(res) > 0:
+        res = json.loads(res)
 
-        f = urlopen(req)
-        res = json.loads(f.read())
-        code = f.getcode()
-        f.close()
-        return code, res
-    except:
-        return 500, dict()
+    return code, res
+
+def get_info(**kwargs):
+    code, info = fetch_url(build_url(kwargs, '/info'))
+
+    if code != 200:
+        raise Exception('Could not retrieve node information')
+
+    return info
+
+def get_swarm_addrs(node_addr):
+    info = get_info(url=node_addr)
+
+    if 'Swarm' not in info or 'RemoteManagers' not in info['Swarm']:
+        return None
+
+    return [manager['Addr'] for manager in info['Swarm']['RemoteManagers']]
+
+def get_join_token(node_addrs, node_type):
+    """Get the join tokens and the swarm addr from the first answering node in a list of remote nodes"""
+    for node_addr in node_addrs:
+        code, info = fetch_url(build_url(dict(url=node_addr), '/swarm'))
+
+        if code != 200:
+            continue
+
+        if 'JoinTokens' not in info:
+            continue
+
+        return info['JoinTokens'][node_type.capitalize()], get_swarm_addrs(node_addr)
+
+    return None, None
 
 # ACTUAL DOCKER METHODS
 
-def init(listen_addr=None, advertise_addr=None, force=False, **kwargs):
+def init(listen_addr, advertise_addr=None, force=False, **kwargs):
     """Init a swarm cluster"""
     post_data = dict(
         ListenAddr=listen_addr,
@@ -167,17 +204,81 @@ def init(listen_addr=None, advertise_addr=None, force=False, **kwargs):
 
     raise Exception('Invalid answer from docker: %s' % (res['message']))
 
+def join(type, remote_addrs, listen_addr, advertise_addr=None, **kwargs):
+    if type not in ['worker', 'manager']:
+        raise Exception("value of type must be one of: worker,manager, got: %s" % (type))
+
+    # find the join token
+    token, swarm_addrs = get_join_token(remote_addrs, type)
+
+    if token == None or len(swarm_addrs) == 0:
+        raise Exception('Could not load swarm information from any of the remote nodes')
+
+    post_data = dict(
+        ListenAddr=listen_addr,
+        RemoteAddrs=swarm_addrs,
+        JoinToken=token
+    )
+
+    if advertise_addr != None and advertise_addr != "":
+        post_data['AdvertiseAddr'] = advertise_addr
+
+    code, res = fetch_url(build_url(kwargs, '/swarm/join'), post_data)
+
+    # already in a swarm
+    if code == 406:
+        return False, res
+
+    if code == 200:
+        return True, res
+
+    raise Exception('Invalid answer from docker: %s' % (res['message']))
+
+def availability(type, **kwargs):
+    if type not in ['active', 'pause', 'drain']:
+        raise Exception("value of type must be one of: active,pause,drain, got: %s" % (type))
+
+    # retrieve the node id
+    info = get_info(**kwargs)
+
+    if 'Swarm' not in info or 'NodeID' not in info['Swarm']:
+        raise Exception('This node is not part of a swarm')
+
+    node_id = info['Swarm']['NodeID']
+
+    code, node_info = fetch_url(build_url(kwargs, '/nodes/%s' % (node_id)))
+
+    if code != 200 or 'Spec' not in node_info:
+        raise Exception('Could not retrieve node information')
+
+    current_type = node_info['Spec']['Availability']
+    current_version = node_info['Version']['Index']
+
+    if type == current_type:
+        return False, None
+
+    post_data = node_info['Spec']
+    post_data['Availability'] = type
+
+    code, res = fetch_url(build_url(kwargs, '/nodes/%s/update?version=%d' % (node_id, current_version)), post_data)
+
+    if code == 200:
+        return True, res
+
+    raise Exception('Invalid answer from docker: %s' % (res['message']))
+
 def main():
     install_opener(build_opener(UnixHandler()))
 
     #argument_spec = url_argument_spec()
     argument_spec = dict(
         url = dict(required=False),
-        action = dict(required=True, choices=['init', 'join', 'leave']),
-        listen_addr = dict(required=True),
+        action = dict(required=True, choices=['init', 'join', 'availability']),
+        listen_addr = dict(required=False),
         advertise_addr = dict(required=False),
         force = dict(required=False, type='bool'),
-        remote_addrs = dict(required=False),
+        remote_addrs = dict(required=False, type='list'),
+        type = dict(required=False)
     )
 
     module = AnsibleModule(argument_spec = argument_spec)
@@ -188,6 +289,10 @@ def main():
         res = None
         if action == 'init':
             changed, res = init(**module.params)
+        elif action == 'join':
+            changed, res = join(**module.params)
+        elif action == 'availability':
+            changed, res = availability(**module.params)
 
         module.exit_json(changed=changed, result=res)
     except Exception as e:
